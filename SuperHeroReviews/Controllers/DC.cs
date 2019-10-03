e443745eca317f544f74a532d0ac30d555a67ca2
William Harris <williamharris2021@gmail.com>
2019-10-03 12:00:16 -0400

    added view

--- SuperHeroReviews/Controllers/DC.cs
+++ SuperHeroReviews/Controllers/DC.cs
@@ -9,5 +9,62 @@ namespace SuperHeroReviews.Controllers
 {
     public class DC : Controller
     {
+        public ViewResult AquaMan()
+        {
+            return View();
+        }
+
+        public ViewResult Batman()
+        {
+            return View();
+        }
+
+        public ViewResult Beastboy()
+        {
+            return View();
+        }
+
+        public ViewResult Cyborg()
+        {
+            return View();
+        }
+
+        public ViewResult Flash()
+        {
+            return View();
+        }
+
+        public ViewResult Joker()
+        {
+            return View();
+        }
+
+        public ViewResult Nightwing()
+        {
+            return View();
+        }
+
+        public ViewResult SuperGirl()
+        {
+            return View();
+        }
+
+        public ViewResult Superman()
+        {
+            return View();
+        }
+
+        public ViewResult Swampthing()
+        {
+            return View();
+        }
+
+        public ViewResult WonderWoman()
+        {
+            return View();
+        }
+
+
+
     }
 }
--- SuperHeroReviews/Controllers/Marvel.cs
+++ SuperHeroReviews/Controllers/Marvel.cs
@@ -8,6 +8,54 @@ namespace SuperHeroReviews.Controllers
 {
     public class Marvel : Controller
     {
+        public ViewResult Antman()
+        {
+            return View();
+        }
 
+        public ViewResult Blackpanther()
+        {
+            return View();
+        }
+
+        public ViewResult BlackWidow()
+        {
+            return View();
+        }
+
+        public ViewResult Ironman()
+        {
+            return View();
+        }
+
+        public ViewResult Magneto()
+        {
+            return View();
+        }
+
+        public ViewResult Mystique()
+        {
+            return View();
+        }
+
+        public ViewResult Spiderman()
+        {
+            return View();
+        }
+
+        public ViewResult Thanos()
+        {
+            return View();
+        }
+
+        public ViewResult Thor()
+        {
+            return View();
+        }
+
+        public ViewResult Wolverine()
+        {
+            return View();
+        }
     }
 }
