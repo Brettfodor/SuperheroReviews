502a4b1f7f4b3d12c8183eb9691032f811ea1da3
Brett Fodor <bmattmusic@gmail.com>
2019-10-03 10:43:39 -0400

    first commit basic framework

---
+++ SuperHeroReviews.Tests/HomeControllerTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroReviews.Controllers;
+
+namespace SuperHeroReviews.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_View()
+        {
+         
+
+        }
+    }
+}
