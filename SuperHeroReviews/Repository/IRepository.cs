e122b8ae918620d0e3d17079b39e1115504815d7
Steven <steventruong1995@yahoo.com>
2019-10-03 15:33:51 -0400

    added repository folder

---
+++ SuperHeroReviews/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroReviews.Repository
+{
+    public class IRepository<T> where T :class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+    }
+}
